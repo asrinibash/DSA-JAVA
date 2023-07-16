4c2e790fa1acf68ee09d3c82fa742f9cd0a235cd
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2023-07-16 23:15:21 +0530

    Add files via upload

---
+++ src/Backtracking/JP_02_NQueens.java
@@ -0,0 +1,71 @@
+package Backtracking;
+
+public class JP_02_NQueens {
+    public static void main(String[] args) {
+        int n = 5;
+        boolean[][] board = new boolean[n][n];
+        System.out.println(queens(board, 0));
+    }
+
+    static int queens(boolean[][] board, int row) {
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return 1;
+        }
+        int count = 0;
+
+        //Placing the Queen and checking every row and column
+        for (int col = 0; col < board.length; col++) {
+
+            //place the queen
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                count += queens(board, row + 1);
+                board[row][col] = false;
+            }
+        }
+        return count;
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        //for vertical check
+        for (int i = 0; i < row; i++) {
+            if (board[i][col]) {
+                return false;
+            }
+        }
+
+        //for diagonal left
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i]) {
+                return false;
+            }
+        }
+
+        //for diagonal right
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i]) {
+                return false;
+            }
+        }
+
+        //if none of above statement is executed then we will assign as true
+        return true;
+    }
+
+    private static void display(boolean[][] board) {
+        for (boolean[] arr : board) {
+            for (boolean element : arr) {
+                if (element) {
+                    System.out.print("Q ");
+                } else {
+                    System.out.print("X ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
