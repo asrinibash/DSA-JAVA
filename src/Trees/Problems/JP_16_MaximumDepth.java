98b3bc8309a36883327db873a95c1e824307ee5d
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2023-11-18 23:24:17 +0530

    solved new tree  questions

---
+++ src/Trees/Problems/JP_16_MaximumDepth.java
@@ -0,0 +1,12 @@
+//package Trees.Problems;
+//Q-104-leetcode
+//public class JP_16_MaximumDepth {
+//    public int maxDepth(TreeNode root) {
+//        if(root==null)
+//            return 0;
+//        int left=maxDepth( root.left);
+//        int right=maxDepth( root.right);
+//
+//        return left>right?left+1:right+1;
+//    }
+//}
